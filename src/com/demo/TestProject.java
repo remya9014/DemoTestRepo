3d088280e82c65382c7f000f97d14d8e5312449c
Rumzz <Rumzz@LAPTOP-OONO2DPD>
2021-03-20 19:05:03 +1100

    First commit

---
+++ src/com/demo/TestProject.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+public class TestProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hi this is remya");
+
+	}
+
+}
